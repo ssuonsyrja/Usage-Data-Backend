c6e8c38c4eb3fb85f57789f6040307e8e9de3071
ssuonsyrja <ssuonsyrja@gmail.com>
2016-09-15 11:01:15 +0300

    SessionID added to schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160907114505) do
+ActiveRecord::Schema.define(version: 20160915075729) do
 
   create_table "events", force: :cascade do |t|
     t.string   "target"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20160907114505) do
     t.string   "targetValue"
     t.string   "targetInnerText"
     t.string   "targetId"
+    t.string   "sessionId"
   end
 
 end
